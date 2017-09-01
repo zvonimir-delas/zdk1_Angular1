097f1b2dc7e8d16dd5a3c30b692d4362ebd6992f
zvonimir-delas <6d.solin@gmail.com>
2017-09-01 12:07:55 +0200

    Added all methods for submitting a new student (to local storage)

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -3,4 +3,25 @@ var app = angular.module('myApp', ['LocalStorageModule']);
 app.run(function(localStorageService){
     if(!localStorageService.get('students'))
         localStorageService.set('students', [{name: 'Marko', surname: 'Marković', sex: 'M', DateOfEntry: new Date(2017, 5, 15, 8, 50, 51)}, {name: 'Jelena', surname: 'Jelenić', sex: 'F', DateOfEntry: new Date(2017, 4, 12, 15, 40, 1)}]);
-    });
+    var students = localStorageService.get('students');    
+});
+
+app.controller('studentController', function($scope, localStorageService){
+    $scope.name;
+    $scope.surname;
+    $scope.sex;
+    $scope.dateOfEntry;
+    $scope.allStudents = localStorageService.get('students');
+
+    $scope.submitNewStudent = function(){
+       $scope.allStudents.push({name: $scope.name, surname: $scope.surname, sex: $scope.sex, dateOfEntry: new Date(Math.random()*new Date())});
+       localStorageService.set('students', $scope.allStudents);
+
+       //thanks to two way data binding, this clears the HTML input
+       $scope.name = null;
+       $scope.surname = null;
+       $scope.sex = null;
+    }
+});
+
+//localStorage.clear();
